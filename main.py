2e2e06245e0e158f99d508628f6d4b8702d5c6d0
riri324 <riyekim135@gmail.com>
2025-01-11 23:30:18 +0900

    postman works now

--- main.py
+++ main.py
@@ -17,14 +17,11 @@ import os
 import json
 import requests
 
-load_dotenv()
-
+load_dotenv(override=True)
 openai.api_key = os.getenv("OPEN_AI_KEY")
 openai.organization = os.getenv("OPEN_AI_ORG")
 elevenlabs_key = os.getenv("ELEVENLABS_KEY")
-
 app = FastAPI()
-
 origins = [
     "http://localhost:5174",
     "http://localhost:5173",
